ed766a0f9bff1aeb516eaa265ae87d9952ae8c40
Jason Guy <jason@aurorai.com>
2026-02-14 17:48:37 +1100

    add start script for render

--- src/server.js
+++ src/server.js
@@ -1373,5 +1373,8 @@ app.get("/", (_req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on :${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
